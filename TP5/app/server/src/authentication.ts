a9abe4631a16efb15762cf306c0080f6cdd131c1
Kevin Pastor <kevin.pastor@polymtl.ca>
2019-02-01 14:34:33 -0500

    Added authentication

---
+++ TP5/app/server/src/authentication.ts
@@ -0,0 +1,11 @@
+const authentication: any = {
+    server: "baasu.db.elephantsql.com (baasu-01)",
+    database: "fffygmfv",
+    username: "fffygmfv",
+    password: "oClmDgoddpzYe1fM1ivS0MJRS3_A4T1Q",
+    url: "postgres://fffygmfv:oClmDgoddpzYe1fM1ivS0MJRS3_A4T1Q@baasu.db.elephantsql.com:5432/fffygmfv",
+    apiKey: "2ddf0762-cd79-4877-ad38-dcfd3e5a58a3",
+};
+
+// https://stackoverflow.com/questions/12663639/how-to-hide-databases-that-i-am-not-allowed-to-access
+// https://www.elephantsql.com/docs/pgadmin.html
